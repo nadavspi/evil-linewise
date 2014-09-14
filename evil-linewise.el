05f2338af0a390e567569bde3f3f5ca1124e5dcf
Nadav Spiegelman <me@nadav.name>
2014-09-14 17:45:32 -0400

    Add [e and ]e with move-text

--- evil-linewise.el
+++ evil-linewise.el
@@ -19,7 +19,20 @@
   (evil-previous-line count)
   (message nil))
 
+(evil-define-command evil-linewise-move-text-down (&optional count argument)
+  "Moves line arg lines down. Uses move-text.el"
+  (interactive "p")
+  (move-text-down count))
+
+(evil-define-command evil-linewise-move-text-up (&optional count argument)
+  "Moves line arg lines up. Uses move-text.el"
+  (interactive "p")
+  (move-text-up count))
+
 (define-key evil-normal-state-map (kbd "[ SPC") 'evil-linewise-blank-lines-above)
 (define-key evil-normal-state-map (kbd "] SPC") 'evil-linewise-blank-lines-below)
+
+(define-key evil-normal-state-map (kbd "[ e") 'evil-linewise-move-text-up)
+(define-key evil-normal-state-map (kbd "] e") 'evil-linewise-move-text-down)
 
 (provide 'evil-linewise)
