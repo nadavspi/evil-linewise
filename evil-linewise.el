51b3ed8ec6e4228f59533b17ac99a783dff4fab8
Nadav Spiegelman <me@nadav.name>
2014-09-14 18:02:33 -0400

    Add buggy implementation of [p and ]p

--- evil-linewise.el
+++ evil-linewise.el
@@ -29,10 +29,31 @@
   (interactive "p")
   (move-text-up count))
 
+(evil-define-command evil-linewise-paste-newline-below
+  "Pastes in a new line below"
+  (interactive)
+  ;; (move-end-of-line 1)
+  ;; (newline-and-indent)
+  (evil-open-below 1)
+  (evil-normal-state)
+  (evil-paste-after 1)
+  (message nil))
+
+(evil-define-command evil-linewise-paste-newline-above
+  "Pastes in a new line above"
+  (interactive)
+  (evil-open-above 1)
+  (evil-normal-state)
+  (evil-paste-after 1)
+  (message nil))
+
 (define-key evil-normal-state-map (kbd "[ SPC") 'evil-linewise-blank-lines-above)
 (define-key evil-normal-state-map (kbd "] SPC") 'evil-linewise-blank-lines-below)
 
 (define-key evil-normal-state-map (kbd "[ e") 'evil-linewise-move-text-up)
 (define-key evil-normal-state-map (kbd "] e") 'evil-linewise-move-text-down)
+
+(define-key evil-normal-state-map (kbd "[ p") 'evil-linewise-paste-newline-above)
+(define-key evil-normal-state-map (kbd "] p") 'evil-linewise-paste-newline-below)
 
 (provide 'evil-linewise)
