c70bf3da439d91462bcb744e00e89202e4d0babc
Nadav Spiegelman <me@nadav.name>
2014-10-06 23:09:04 -0400

    Improve namespacing

--- evil-linewise.el
+++ evil-linewise.el
@@ -1,9 +1,9 @@
 (defgroup evil-linewise nil
   "Useful linewise mappings for Evil inspired by unimpaired.vim"
-  :prefix "evil-linewise-"
+  :prefix "evil-linewise/"
   :group 'evil)
 
-(evil-define-command evil-linewise-blank-lines-above (&optional count argument)
+(evil-define-command evil-linewise/blank-lines-above (&optional count argument)
   "Inserts blank lines above the cursor"
   (interactive "p")
   (evil-open-above count)
@@ -11,7 +11,7 @@
   (evil-next-line)
   (message nil))
 
-(evil-define-command evil-linewise-blank-lines-below (&optional count argument)
+(evil-define-command evil-linewise/blank-lines-below (&optional count argument)
   "Inserts blank lines below the cursor"
   (interactive "p")
   (evil-open-below count)
@@ -19,17 +19,17 @@
   (evil-previous-line count)
   (message nil))
 
-(evil-define-command evil-linewise-move-text-down (&optional count argument)
+(evil-define-command evil-linewise/move-text-down (&optional count argument)
   "Moves line arg lines down. Uses move-text.el"
   (interactive "p")
   (move-text-down count))
 
-(evil-define-command evil-linewise-move-text-up (&optional count argument)
+(evil-define-command evil-linewise/move-text-up (&optional count argument)
   "Moves line arg lines up. Uses move-text.el"
   (interactive "p")
   (move-text-up count))
 
-(evil-define-command evil-linewise-paste-newline-below
+(evil-define-command evil-linewise/paste-newline-below
   "Pastes in a new line below"
   (interactive)
   ;; (move-end-of-line 1)
@@ -39,7 +39,7 @@
   (evil-paste-after 1)
   (message nil))
 
-(evil-define-command evil-linewise-paste-newline-above
+(evil-define-command evil-linewise/paste-newline-above
   "Pastes in a new line above"
   (interactive)
   (evil-open-above 1)
@@ -48,35 +48,30 @@
   (message nil))
 
 ;; From: https://github.com/cofi/dotfiles/blob/master/emacs.d/config/cofi-evil.el
-(defun cofi/copy-previous-line (count)
+(defun evil-linewise/copy-previous-line (count)
   "Copy the line `count' before to the current."
   (interactive "p")
   (save-excursion
     (forward-line (- count))
     (evil-yank-line (point-at-bol) (point-at-eol) 'line)))
 
-(defun cofi/copy-next-line (count)
+(defun evil-linewise/copy-next-line (count)
   "Copy the line `count' after to the current."
   (interactive "p")
   (save-excursion
     (forward-line count)
     (evil-yank-line (point-at-bol) (point-at-eol) 'line)))
 
-(define-key evil-normal-state-map (kbd "[ SPC") 'evil-linewise-blank-lines-above)
-(define-key evil-normal-state-map (kbd "] SPC") 'evil-linewise-blank-lines-below)
+(define-key evil-normal-state-map (kbd "[ SPC") 'evil-linewise/blank-lines-above)
+(define-key evil-normal-state-map (kbd "] SPC") 'evil-linewise/blank-lines-below)
 
-(define-key evil-normal-state-map (kbd "[ e") 'evil-linewise-move-text-up)
-(define-key evil-normal-state-map (kbd "] e") 'evil-linewise-move-text-down)
+(define-key evil-normal-state-map (kbd "[ e") 'evil-linewise/move-text-up)
+(define-key evil-normal-state-map (kbd "] e") 'evil-linewise/move-text-down)
 
-(define-key evil-normal-state-map (kbd "[ p") 'evil-linewise-paste-newline-above)
-(define-key evil-normal-state-map (kbd "] p") 'evil-linewise-paste-newline-below)
+(define-key evil-normal-state-map (kbd "[ p") 'evil-linewise/paste-newline-above)
+(define-key evil-normal-state-map (kbd "] p") 'evil-linewise/paste-newline-below)
 
-(define-key evil-normal-state-map (kbd "[ y") 'cofi/copy-previous-line)
-(define-key evil-normal-state-map (kbd "] y") 'cofi/copy-next-line)
-
-;; Make "c" a prefix
-;;(define-key evil-motion-state-map "cu" 'universal-argument)
-
-;;(define-key evil-normal-state-map (kbd "c o n") 'linum-mode)
+(define-key evil-normal-state-map (kbd "[ y") 'evil-linewise/copy-previous-line)
+(define-key evil-normal-state-map (kbd "] y") 'evil-linewise/copy-next-line)
 
 (provide 'evil-linewise)
